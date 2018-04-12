d631992d300b90c6ebf60eaaae6504631ddb6eb5
klozz <klozz707@gmail.com>
2018-04-12 06:33:14 -0400

    regenerate makefiles
    Signed-off-by: klozz <klozz707@gmail.com>

--- land/Android.mk
+++ land/Android.mk
@@ -348,6 +348,7 @@ LOCAL_SRC_FILES := proprietary/vendor/etc/qdcm_calib_data_hx8394F_HD720p_video_B
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .xml
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -357,6 +358,7 @@ LOCAL_SRC_FILES := proprietary/vendor/etc/qdcm_calib_data_ili9881c_HD720p_video_
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .xml
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -366,6 +368,7 @@ LOCAL_SRC_FILES := proprietary/vendor/etc/qdcm_calib_data_nt35521s_HD720p_video_
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_SUFFIX := .xml
+LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
 endif
