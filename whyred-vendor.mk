3a3ec42f12b0864886e11d82dc2997bf698aabb8
Akhil Narang <akhilnarang.1999@gmail.com>
2018-08-19 21:34:36 +0530

    whyred: add libskia
      * libMiWatermark needs it
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- whyred-vendor.mk
+++ whyred-vendor.mk
@@ -100,6 +100,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/whyred/proprietary/lib/librcc.so:system/lib/librcc.so \
     vendor/xiaomi/whyred/proprietary/lib/libsd_sdk_display.so:system/lib/libsd_sdk_display.so \
     vendor/xiaomi/whyred/proprietary/lib/libsdm-disp-apis.so:system/lib/libsdm-disp-apis.so \
+    vendor/xiaomi/whyred/proprietary/lib/libskia.so:system/lib/libskia.so \
     vendor/xiaomi/whyred/proprietary/lib/libtrueportrait.so:system/lib/libtrueportrait.so \
     vendor/xiaomi/whyred/proprietary/lib/libvendorconn.so:system/lib/libvendorconn.so \
     vendor/xiaomi/whyred/proprietary/lib/libwfdavenhancements.so:system/lib/libwfdavenhancements.so \
